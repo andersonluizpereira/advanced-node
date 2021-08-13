db1a8c12e2778dff0bbd53db8e0ccf29682537f4
Anderson Pereira <andersononus@hotmail.com>
2021-08-13 14:59:00 -0300

    isolate sut creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -9,23 +9,26 @@ class AxiosHttpClient {
   }
 }
 
+let sut: AxiosHttpClient
+let fakeAxios: jest.Mocked<typeof axios>
+let url: string
+let params: object
+
+beforeAll(() => {
+  url = 'any_url'
+  params = { any: 'any' }
+  fakeAxios = axios as jest.Mocked<typeof axios>
+})
+
+beforeEach(() => {
+  sut = new AxiosHttpClient()
+})
+
 describe('AxiosHttpClient', () => {
   describe('#get', () => {
-    it('', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
-
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any'
-        }
-      })
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any'
-        }
-      })
+    it('should call get with correct values', async () => {
+      await sut.get({ url, params })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
