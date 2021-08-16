23040a2416fc28aa715b8784b37791b37268a5dd
Anderson Pereira <andersononus@hotmail.com>
2021-08-16 18:28:05 -0300

    isolate SUT creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -1,9 +1,7 @@
 import { TokenGenerator } from '@/data/contracts/crypto'
 
 import jwt from 'jsonwebtoken'
-
 jest.mock('jsonwebtoken')
-
 describe('JwtTokenGenerator', () => {
   class JwtTokenGenerator {
     constructor (private readonly secret: string) {}
@@ -14,9 +12,17 @@ describe('JwtTokenGenerator', () => {
     }
   }
 
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret')
+  })
+
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
   it('should call sign with correct values', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
   })
