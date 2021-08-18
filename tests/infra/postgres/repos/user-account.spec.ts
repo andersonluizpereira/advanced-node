92f8053c3226baa44223464ea2a5885d2566626d
Anderson Pereira <andersononus@hotmail.com>
2021-08-18 18:59:37 -0300

    move inMemoryDb creation to its own file

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -34,7 +34,6 @@ describe('PgUserAccountRepository', () => {
     })
 
     it('should return undefined if email does not exists', async () => {
-      const sut = new PgUserAccountRepository()
       const account = await sut.load({ email: 'new_email' })
       expect(account).toBeUndefined()
     })
