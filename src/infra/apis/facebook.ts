0bdae9a50f1a713d2ca552b511c7a9d9c60d140d
Anderson Pereira <andersononus@hotmail.com>
2021-09-06 10:19:58 -0300

    ensure FacebookApi returns undefined when HttpGetClient throws

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -34,7 +34,7 @@ export class FacebookApi implements LoadFacebookUserApi {
         name: userInfo.name,
         email: userInfo.email
       }
-    } catch (error) {
+    } catch {
       return undefined
     }
   }
