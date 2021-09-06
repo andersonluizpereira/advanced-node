4f64c398ad959ade4ee6666a4b65befa5705ff36
Anderson Pereira <andersononus@hotmail.com>
2021-09-06 10:10:32 -0300

    ensure FacebookApi returns undefined when HttpGetClient

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '371637644708952',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '9225a254d156931c85a32ebc6e7c128e'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,31 @@
-describe('', () => {
-  it('teste', () => {
-    expect(1).toBe(1)
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('Facebook Api Integration Test', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+    const fbUser = await sut.loadUser({ token: 'EAAFSAKHMTFgBAEQHZCBvDMlXViMBzcpnf58Il0Wz3pqKqx8hWPZAi67Qtaxc7MgOZBPUIOpb9ReYdveIz1eVMku0XI95IWXffvUuvYk2p2gvk1swQxYH2MFzsB5FsSorZAQ0tR5l0h1lkcLbwTbcLaBZAllkIKgyLDRTgApsKiVlAXiaY9oZCbZAa7II84tbb7ZBM9TF1kVFEtTeHBfQuf5ZB' })
+    expect(fbUser).toEqual({
+      facebookId: '108749701544034',
+      email: 'anderson_rsqjwqs_teste@tfbnw.net',
+      name: 'Anderson Teste'
+    })
+  })
+
+  it('should return a Facebook User if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+    expect(fbUser).toBeUndefined()
   })
 })
