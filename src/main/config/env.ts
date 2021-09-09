def2435fa3c745d8d92cc4a2fe3f6500a4637869
Anderson Pereira <andersononus@hotmail.com>
2021-09-09 11:36:43 -0300

    configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '371637644708952',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '9225a254d156931c85a32ebc6e7c128e'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
---
+++ src/main/routes/login.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/api/login/facebook', (req, res, next) => {
+    res.send({ data: 'any_data' })
+  })
+}
