5e9d5ecbb66638c5945bc6a40e4d79678f30b768
Anderson Pereira <andersononus@hotmail.com>
2021-09-17 10:23:08 -0300

    connect to postgres db before server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,11 @@
 import './config/module-alias'
-
-import 'reflect-metadata'
-
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { config } from '@/infra/postgres/helpers'
 
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
+createConnection(config)
+  .then(() => app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)))
+  .catch(console.error)
