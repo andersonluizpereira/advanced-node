195a495471388ba798bc617109905bf4cee5f575
Anderson Pereira <andersononus@hotmail.com>
2021-09-23 17:15:43 -0300

    convert FacebookAuthenticationUseCase into a function

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '371637644708952',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '9225a254d156931c85a32ebc6e7c128e'
+    clientId: process.env.FB_CLIENT_ID ?? '255017709763229',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? 'c02b1e197ee6aec2947b193148cdaef7'
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? '3jk24h32jk4h'
