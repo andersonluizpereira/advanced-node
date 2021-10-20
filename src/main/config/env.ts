82840fd2fb0f66416176aa94b76ba85b390b961b
Anderson Pereira <andersononus@hotmail.com>
2021-10-20 09:33:41 -0300

    ensure auth middleware returns 200 authorization header is valid

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,8 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
-    accessToken: process.env.FB_ACCESS_TOKEN ?? ''
+    clientId: process.env.FB_CLIENT_ID ?? '371637644708952',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '9225a254d156931c85a32ebc6e7c128e'
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? ''
+  jwtSecret: process.env.JWT_SECRET ?? '3jk24h32jk4h'
 }
--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,8 +1,10 @@
 import { ForbiddenError } from '@/application/errors'
 import { app } from '@/main/config/app'
 import { auth } from '@/main/middlewares'
+import { sign } from 'jsonwebtoken'
 
 import request from 'supertest'
+import { env } from '@/main/config/env'
 
 describe('Authentication Middleware', () => {
   it('should return 403 if authorization header was not provided', async () => {
@@ -11,5 +13,18 @@ describe('Authentication Middleware', () => {
 
     expect(status).toBe(403)
     expect(body.error).toBe(new ForbiddenError().message)
+  })
+
+  it('should return 200 if authorization header is valid', async () => {
+    const authorization = sign({ key: 'any_user_id' }, env.jwtSecret)
+    app.get('/fake_route', auth, (req, res) => {
+      res.json(req.locals)
+    })
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization })
+
+    expect(status).toBe(200)
+    expect(body).toEqual({ userId: 'any_user_id' })
   })
 })
