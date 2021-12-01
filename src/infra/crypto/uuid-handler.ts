e470b38c0579b4829e4e9794184224584b5459e7
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2021-12-01 18:26:21 -0300

    move UUIDHandler to its own file

---
+++ src/infra/crypto/uuid-handler.ts
@@ -0,0 +1,8 @@
+import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { v4 } from 'uuid'
+
+export class UUIDHandler {
+  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
+    return `${key}_${v4()}`
+  }
+}
