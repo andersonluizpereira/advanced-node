7ddbacfc961891c32241922c8e187896b0b6ff72
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2021-12-01 18:19:26 -0300

    isolate SUT creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -11,17 +11,22 @@ class UUIDHandler {
 }
 
 describe('UUIDHandler', () => {
-  it('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+
+  beforeAll(() => {
+    mocked(v4).mockReturnValue('any_uuid')
+  })
+
+  it('should call uuid.v4', () => {
     sut.uuid({ key: 'any_key' })
     expect(v4).toHaveBeenCalledTimes(1)
   })
 
   it('should return correct uuid', () => {
-    mocked(v4).mockReturnValueOnce('any_uuid')
-    const sut = new UUIDHandler()
-
     const uuid = sut.uuid({ key: 'any_key' })
     expect(uuid).toBe('any_key_any_uuid')
   })
