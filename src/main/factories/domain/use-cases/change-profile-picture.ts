106060e1d4ba91718d29a0e0f72bba030099b90c
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-01-20 14:43:06 -0300

    add delete user picture integration test

---
+++ src/main/factories/domain/use-cases/change-profile-picture.ts
@@ -0,0 +1,10 @@
+import { setupChangeProfilePicture, ChangeProfilePicture } from '@/domain/use-cases'
+import { makeAwsS3FileStorage, makeUniqueId } from '@/main/factories/infra/gateways'
+import { makePgUserProfileRepo } from '@/main/factories/infra/repos/pg-user-profile'
+export const makeChangeProfilePicture = (): ChangeProfilePicture => {
+  return setupChangeProfilePicture(
+    makeAwsS3FileStorage(),
+    makeUniqueId(),
+    makePgUserProfileRepo()
+  )
+}
--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -3,16 +3,20 @@ import { app } from '@/main/config/app'
 import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 
 import { IBackup } from 'pg-mem'
-import { getConnection } from 'typeorm'
+import { getConnection, getRepository, Repository } from 'typeorm'
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+import { env } from '@/main/config/env'
 
 describe('Login Routes', () => {
   describe('POST /login/facebook', () => {
     let backup: IBackup
+    let pgUserRepo: Repository<PgUser>
 
     beforeAll(async () => {
       const db = await makeFakeDb([PgUser])
       backup = db.backup()
+      pgUserRepo = getRepository(PgUser)
     })
 
     afterAll(async () => {
@@ -28,6 +32,17 @@ describe('Login Routes', () => {
         .delete('/api/users/picture')
 
       expect(status).toBe(403)
+    })
+
+    it('should return 204', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email' })
+      const authorization = sign({ key: id }, env.jwtSecret)
+      const { status, body } = await request(app)
+        .delete('/api/users/picture')
+        .set({ authorization })
+
+      expect(status).toBe(204)
+      expect(body).toEqual({})
     })
   })
 })
