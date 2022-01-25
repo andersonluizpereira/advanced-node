8331c64268c49688f62de8c9cd8aa7b18201ba7f
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-01-25 15:22:58 -0300

    merge delete and save picture controllers

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
 import { adaptExpressRoute as adapt } from '@/main/adapters'
-import { makeDeletePictureController } from '@/main/factories/application/controllers'
+import { makeSavePictureController } from '@/main/factories/application/controllers'
 
 export default (router: Router): void => {
-  router.delete('/users/picture', auth, adapt(makeDeletePictureController()))
+  router.delete('/users/picture', auth, adapt(makeSavePictureController()))
+  router.put('/users/picture', auth, adapt(makeSavePictureController()))
 }
